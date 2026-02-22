6dc6240b28d3e240fc52b3b0a99b6010875a1f5a
jardessonrb <jardessonrs1117@gmail.com>
2026-02-21 22:10:15 -0300

    Adicionada variável de ambiente para a API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.200.103:8080", 
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: "http://192.168.200.103:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
 });
 
